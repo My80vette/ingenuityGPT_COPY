1839762fc71ce32cd988025644b673ebb4a0555a
Stone Mitchell <stone.mitchell.45@gmail.com>
2024-01-24 22:19:31 -0700

    added message box and functional send button

--- streamlit/smeui.py
+++ streamlit/smeui.py
@@ -1,29 +1,32 @@
 import streamlit as st
 
-st.title('Mars Engenuity Subject Matter Expert')
+st.title('Mars Ingenuity SME')
 
 chatTab, chatHistoryTab = st.tabs(["Chat", "Chat History"])
 
 # main view with text-message style display of current model conversation.
 with chatTab:
-    # temp code from copied example
-
-    st.write('This is the chat view.')
-    # st.header("A cat")
-    # t.image("", width=200)
+    messageCol, sendCol = st.columns([6, 1])
+    with messageCol:
+        st.text_area('', key='message')
+    with sendCol:
+        st.button('Send', key='send')
+    if st.session_state.get('send'):
+        st.write(st.session_state['message'])
+            
+        
 
 # view of previous, possibly continuable chats (stretch goal)
 with chatHistoryTab:
-    # temp code from copied example
-    
     st.write('This is the chat history view.')
-    # st.header("A dog")
-    # st.image("", width=200)
 
-# playing with button functionality (learning as I go, this is the first commit)
+    # if st.session_state.get('clear'):
+    #     st.session_state['name'] = ''
+    # if st.session_state.get('streamlit'):
+    #     st.session_state['name'] = 'Streamlit'
 
-# st.button("Reset", type="primary")
-# if st.button('Say hello'):
-#     st.write('Why hello there')
-# else:
-#     st.write('Goodbye')
+    # st.text_input('Name', key='name')
+
+    # st.button('Clear name', key='clear')
+    # st.button('Streamlit!', key='streamlit')
+
